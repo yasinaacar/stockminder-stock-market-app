1cb61e14c6ae287160ba4a6d1a6a21ff667a1190
yasinaacar <yasinaacar@outlook.com>
2024-08-10 22:21:04 +0300

    basic stock-app

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,32 @@
+class HomeController < ApplicationController
+  def index
+  #   @api_key = "HXAFHCJ2X137DD3U"
+  #   @client = Alphavantage::Client.new key: "HXAFHCJ2X137DD3U"
+  #   @client.verbose = true
+  #   @say_hi = "Hİ"
+  #   @stocks_found = @client.search keywords: "Facebook"
+  #   @stock = @client.stock symbol: "MSFT"
+  #   @stock = Alphavantage::Stock.new symbol: "MSFT", key: "HXAFHCJ2X137DD3U"
+  #   @stock_quote = @stock.quote
+
+  #   puts "----------VERİ GELDİ----------------"
+  #   puts params[:ticker]
+  #   if params[:ticker].present?
+  #     puts params[:ticker]
+  #     @stocks_found = @client.search keywords: "Facebook"
+  #     @stocks_found.output
+  #     puts "/////////////////////////////"
+  #     puts @stocks_found.stocks[0].name
+  #     puts "/////////////////////////////"
+  #   elsif params[:ticker] == ""
+  #     puts "NOTHİNG"
+  #     @nothing = "Hey puts some in form !!!"
+  #   end
+  end
+
+  def about
+  end
+
+
+
+end
