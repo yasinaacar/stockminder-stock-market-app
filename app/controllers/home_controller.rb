aca7981c2d7546360862485c3fa0571850042e15
yasinaacar <yasinaacar@outlook.com>
2024-08-14 09:57:24 +0300

    last-update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,21 +7,18 @@ class HomeController < ApplicationController
       @api = FinnhubRuby::DefaultApi.new
       begin
         @present = @api.company_profile2(symbol: params[:ticker])
-        @stock = @api.quote(params[:ticker])
-        puts @present
-        puts "////////////////////////"
-        puts @stock
-        puts "/////////////////////////"
-
-        if @present.name.blank?
-          @error = "Hey! That Stock Symbol Doesn't Exist. Please Try Again."
+        unless @present.name.present?
+          @error = "Böyle bir hisse senendi bulunamadı"
+        else
+          @stock = @api.quote(params[:ticker])
         end
+        puts @present.name.present?
+
+
       rescue FinnhubRuby::ApiError => e
         @error = "API Error: #{e.message}"
       end
-    elsif params[:ticker].blank?
-      @nothing = "Hey! You Forgot To Enter A Symbol"
-      puts @nothing
+
     end
   end
 
